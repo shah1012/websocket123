9af89ab4a9a4ae179a6c705c2f596b840ad995f7
shah1012 <69828183+shah1012@users.noreply.github.com>
2020-11-05 17:35:45 -0600

    wow

--- mn.js
+++ mn.js
@@ -8,8 +8,8 @@ const server = express().listen(Port, () => {
 
 const wss = new WebSocket.Server({ server: server });
 
-wss.on("connection", (prt) => {
-  console.log(`Wss server listening to port ${prt}`);
+wss.on("listening", (server) => {
+  console.log(`Wss server listening to port ${server}`);
 });
 
 wss.on("connection", (ws) => {
